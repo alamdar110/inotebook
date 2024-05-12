812832c6b5bf7ea5b537a785b75fbcbe3247c4de
alamdar110 <52681801+alamdar110@users.noreply.github.com>
2024-05-13 00:07:48 +0500

    minor change

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/admin';
+const mongoURI = 'mongodb://localhost:27017';
 
 const connectToMongo = async() => {
     try {
         mongoose.set("strictQuery", false);
-        await mongoose.connect(mongoURI);
+        mongoose.connect(mongoURI);
         console.log("Connected to mongoDB successfully");
     } catch (error) {
         console.log(error);
