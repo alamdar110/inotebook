def4eeb2696e8e2cfe9cbd3345e54a7bc4489f02
alamdar110 <52681801+alamdar110@users.noreply.github.com>
2024-05-15 18:40:10 +0500

    express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
+
 // Define routes using the router
 
-router.post('/', async(req, res) => {
-    try {
-        // Create a new user instance with data from the request body
+router.post('/', [
+    body('username', 'Enter a valid username').isLength({ min: 3 }).isString(),
+    body('email', 'Please provide valid Email').isEmail(),
+    body('password', 'Please provide valid Password').isLength({ min: 6 }).isString() // Password must be at least 6 characters long
+], async(req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({ errors: result.array() });
+    } else {
         const user = new User(req.body);
-        // Save the user to the database
-        await user.save();
-        res.json("Form Submitted");
-    } catch (error) {
-        // If there's an error, send an error response
-        res.status(500).json({ error: error.message });
+        await user.save({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+
+        }).then((data) => {
+            res.json("Form Submitted");
+        }).catch((error) => {
+            res.status(500).json({ error: error.errorResponse.errmsg });
+        });
     }
 });
 
