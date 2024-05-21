91d1e08c541b3f699ace6c7b02e0d4307868a083
alamdar110 <52681801+alamdar110@users.noreply.github.com>
2024-05-21 15:25:23 +0500

    minor logic change

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,26 +5,40 @@ const User = require('../models/User');
 
 // Define routes using the router
 
-router.post('/', [
+router.post('/create', [
     body('username', 'Enter a valid username').isLength({ min: 3 }).isString(),
     body('email', 'Please provide valid Email').isEmail(),
     body('password', 'Please provide valid Password').isLength({ min: 6 }).isString() // Password must be at least 6 characters long
 ], async(req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.array() });
-    } else {
-        const user = new User(req.body);
-        await user.save({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
+    try {
+        let user = await User.findOne({
+            $or: [
+                { username: req.body.username },
+                { email: req.body.email }
+            ]
+        })
 
-        }).then((data) => {
-            res.json("Form Submitted");
-        }).catch((error) => {
-            res.status(500).json({ error: error.errorResponse.errmsg });
-        });
+        if (user) {
+            return res.status(400).send("User already exists");
+        } else {
+
+        }
+
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        } else {
+            user = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            });
+            await user.save();
+            return res.status(200).json({ message: "User created successfully", user }); // Include user data in the response
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
     }
 });
 
